5cd2af3f9fe369b55200b100008ac9e10a45cbff
beansprout88 <daniel@tangle.org.uk>
2024-04-07 20:26:02 +0200

    fix

---
+++ R/interactive_data_select.R
@@ -0,0 +1,42 @@
+#' Interactively select intensity data using a lasso gate
+#'
+#' @param data data
+#' @param channel_1 channel 1
+#' @param channel_2 channel 2
+#'
+#' @return Coordinates for gate
+#' @export
+interactive_data_select <-
+  function(data, channel_1, channel_2) {
+
+    ui = miniUI::miniPage(
+      miniUI::gadgetTitleBar("Daniel's interactive data select"),
+      # Render plot
+      plotly::plotlyOutput("plot"))
+
+
+    server = function(input, output) {
+
+      output$plot <- plotly::renderPlotly({
+        p = plot_intensity_2d(data, !!ensym(channel_1),!!ensym(channel_2))
+        plotly::ggplotly(p) %>% plotly::layout(dragmode = 'lasso') %>% plotly::event_register("plotly_brushed")
+      })
+
+      brushed_values = shiny::reactive(plotly::event_data("plotly_brushed"))
+
+      shiny::observe({
+        if(input$done){
+          shiny::stopApp(brushed_values())}
+      })
+
+      shiny::observeEvent(input$cancel, {
+        shiny::stopApp()
+      })
+
+
+    }
+
+    miniUI::runGadget(ui, server)
+
+  }
+
---
+++ R/plot_intensity_2d.R
@@ -0,0 +1,41 @@
+
+#' Interactive plot of 2d intensity data
+#'
+#' @param data
+#' @param channel_1
+#' @param channel_2
+#' @param bins
+#' @param plot_all If FALSE, takes subsample of 500,000 data points to plot
+#' @param plotly Make plot interactive using plotly
+#'
+#' @export
+plot_intensity_2d = function(data, channel_1, channel_2, bins = 500, plot_all = FALSE, plotly = T){
+
+  data = data %>%
+    dplyr::ungroup()
+
+  if(plot_all == FALSE & nrow(data) > 500000){
+    data = data %>%
+      dplyr::sample_n(500000)
+  }
+
+  lim_x = data %>%
+    pull(!!ensym(channel_1)) %>%
+    quantile(c(0.0002, 0.9998), na.rm = T)
+
+  lim_y = data %>%
+    pull(!!ensym(channel_2)) %>%
+    quantile(c(0.0002, 0.9998), na.rm = T)
+
+  plot = data %>%
+    ggplot2::ggplot() +
+    ggplot2::aes(x = !!ensym(channel_1), y = !!ensym(channel_2), fill = sqrt(..count..)) +
+    ggplot2::geom_bin2d(bins = bins) +
+    viridis::scale_fill_viridis() +
+    ggplot2::lims(x = lim_x, y = lim_y) +
+    ggplot2::theme_bw()
+
+  if(plotly) plot = plotly::ggplotly(plot)
+
+  return(plot)
+}
