cb6f9ae3e21b384be82ec369cfc1f30933c5012b
Daniel Greenwood <117200027+danielgreenwood@users.noreply.github.com>
2025-03-23 09:32:46 +0100

    Updated test for Keras functionality.R

--- tests/testthat/test.R
+++ tests/testthat/test.R
@@ -1,3 +1,13 @@
-test_that("multiplication works", {
-  expect_equal(2 * 2, 4)
+test_that("Keras and TensorFlow are installed and working", {
+  skip_if_not_installed("keras")
+
+  expect_silent({
+    library(keras)
+    library(tensorflow)
+
+    # Check if TensorFlow is available and working
+    tf <- tensorflow::tf_config()
+    expect_true(!is.null(tf$version), info = "Keras or Tensorflow not installed. Run keras::install_keras()")
+
+  })
 })
